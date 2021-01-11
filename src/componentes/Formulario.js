ce20c8bb39cb4aea9cc853037b2e2e1b9fcdb16c
Miriam-use <56627040+Miriam-use@users.noreply.github.com>
2021-01-11 18:01:02 +0100

    buscador

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -5,89 +5,89 @@ class Formulario extends Component{
 
     constructor(props){
         super(props);
-
+    
         this.state = {
-            nombre: '',
-            correo: '',
-            edad: '',
-            mensajeNombre: '',
-            mensajeCorreo: '',
-            mensajeEdad:'',
-            validNombre: false,
-            validCorreo: false,
-            validEdad: false,
+          nombre: '',
+          correo: '',
+          edad: '',
+          mensajeNombre: '',
+          mensajeCorreo: '',
+          mensajeEdad: '',
+          invalidNombre: false,
+          invalidCorreo: false,
+          invalidEdad: false,
         }
-
         this.onChange = this.onChange.bind(this);
         this.enviarAlaBD = this.enviarAlaBD.bind(this);
-    }
-
-    onChange(e){
-        let name = e.target.name;
-        let value = e.target.value;
-
-        this.serState({
-            [name]: value,
+      }
+    
+      onChange = e =>{
+        const {name, value} = e.target;
+        this.setState({
+          [name]: value,
         });
-    }
-
-    enviarAlaBD(e){
+      }
+      enviarAlaBD = e  => {
         e.preventDefault();
+        let valido = true;
         if(this.state.nombre === ''){
-            this.setState({
-                validNombre: true,
-                mensajeNombre: 'El campo nombre es obligatorio',
-            });
+          this.setState({
+            invalidNombre: true,
+            mensajeNombre: 'El campo nombre es obligatorio, indica tu nombre',
+          });
+          valido = false;
         }
-
         if(this.state.correo === ''){
-            this.setState({
-                validCorreo: true,
-                mensajeCorreo: 'El campo correo es obligatorio',
-            });
+          this.setState({
+            invalidCorreo: true,
+            mensajeCorreo: 'Indica tu dirección de correo',
+          });
+          valido = false;
         }
-
         if(this.state.edad === ''){
-            this.setState({
-                validEdad: true,
-                mensajeEdad: 'Indica tu edad',
-            });
+          this.setState({
+            invalidEdad: true,
+            mensajeEdad: 'Indica tu edad',
+          });
+          valido = false;
         }
-    }
-
-    render() {
-        return(
-            <div >
-                <Row>
-                    <Col xs="3"></Col>
-                    <Col xs="6">
-                    <h2 className="lead text-center">Formulario de Contacto</h2>
-                        <Form onSubmit={this.enviarAlaBD}>
-                            <FormGroup>
-                                <Label>Nombre</Label>
-                                <Input type="text" name="nombre" value={this.state.nombre} onChange={this.onChange} invalid={this.state.validNombre} />
-                                <FormFeedback>{this.state.mensajeNombre}</FormFeedback>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label>Correo</Label>
-                                <Input type="email" name="correo" value={this.state.correo} onChange={this.onChange} invalid={this.state.validCorreo} />
-                                <FormFeedback>{this.state.mensajeCorreo}</FormFeedback>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label>Edad</Label>
-                                <Input type="text" name="edad" className="col-2" value={this.state.edad} onChange={this.onChange} invalid={this.state.validEdad} />
-                                <FormFeedback>{this.state.mensajeEdad}</FormFeedback>
-                            </FormGroup>
-                            <FormGroup>
-                                <Button color="success">Enviar</Button>
-                            </FormGroup>
-                        </Form>
-                    </Col>
-                </Row>
-            </div>
-            
-        )
-    }
+        if(valido){
+          //Enviarlo a la base de datos o a otro componente
+          console.log("Se envian los datos " + JSON.stringify(this.state));
+        }
+      }
+      render(){
+        return (
+          <div>
+            <Row>
+              <Col xs="3"></Col>
+              <Col xs="6">
+                <h2>Registro de Participantes</h2>
+                <Form onSubmit={this.enviarAlaBD}>
+                  <FormGroup>
+                    <Label>Nombre</Label>
+                    <Input type="text" name="nombre" value={this.state.nombre} onChange={this.onChange} invalid={this.state.invalidNombre} />
+                    <FormFeedback>{this.state.mensajeNombre}</FormFeedback>
+                  </FormGroup>
+                  <FormGroup>
+                    <Label>Correo</Label>
+                    <Input type="email" name="correo" value={this.state.correo} onChange={this.onChange} invalid={this.state.invalidCorreo}/>
+                    <FormFeedback>{this.state.mensajeCorreo}</FormFeedback>
+                  </FormGroup>
+                  <FormGroup>
+                    <Label>Edad</Label>
+                    <Input type="text" name="edad" className="col-2" value={this.state.edad} onChange={this.onChange} invalid={this.state.invalidEdad} />
+                    <FormFeedback>{this.state.mensajeEdad}</FormFeedback>
+                  </FormGroup>
+                  <FormGroup>
+                    <Button color="success">Enviar</Button>
+                  </FormGroup>
+                </Form>
+              </Col>
+            </Row>
+          </div>
+        );
+      }
 }
 
 export default Formulario;
